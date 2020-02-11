a63cbb92a03c2cf546710eac1044d5c2006b9f1d
helciohjr <helcio_0011@hotmail.com>
2020-02-10 22:29:29 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,14 +1,21 @@
 package Application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		
-		Department obj = new Department();
-		System.out.println(obj);
+		Department obj = new Department(2, "books");
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
+		
+		
 	}
 
 }
